9f372409a7e2e9e5fa232f106d280c44f312cabb
Daniel Nunes <danielzcomputacao@gmail.com>
2021-04-20 18:29:48 -0300

    server

--- podecastrnext/src/pages/index.tsx
+++ podecastrnext/src/pages/index.tsx
@@ -1,6 +1,24 @@
 
-export default function Home() {
+
+
+export default function Home(props) {
   return (
-    <div>Index</div>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+return {
+  props: {
+    episodes: data, 
+  
+    },
+    revalidate : 60 * 60 * 8 ,
+  }
 }
