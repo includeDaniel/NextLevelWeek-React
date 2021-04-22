1fac5647e7b5cf5497a54e3921b0e1c02386a14f
Daniel Nunes <danielzcomputacao@gmail.com>
2021-04-22 19:20:16 -0300

    typescript

--- podecastrnext/src/pages/index.tsx
+++ podecastrnext/src/pages/index.tsx
@@ -1,7 +1,21 @@
+import { GetStaticProps } from 'next'
+import { api } from '../services/api'
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { convertDurationToTimeString } from '../utils/converDurationToTimeString'
 
+type Episode = {
+    id: string;
+    title: string;
+    members: string;
+    published_at: string;
+}
 
+type HomeProps = {
+  episodes: Episode[];
+}
 
-export default function Home(props) {
+export default function Home(props : HomeProps) {
   return (
     <div>
       <h1>Index</h1>
@@ -10,13 +24,33 @@ export default function Home(props) {
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export const getStaticProps : GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode => {
+
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  })
 
 return {
   props: {
-    episodes: data, 
+    episodes, 
   
     },
     revalidate : 60 * 60 * 8 ,
